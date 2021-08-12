2971088060630afc001f98611836153e5cf8e016
RavindraKatam88 <ravindra.katam88@gmail.com>
2021-08-12 21:40:35 +0800

    revert back docker file and updated to hostname

--- order-service/src/main/java/com/practice/controller/OrderController.java
+++ order-service/src/main/java/com/practice/controller/OrderController.java
@@ -2,12 +2,14 @@ package com.practice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practice.dto.Product;
 import com.practice.proxy.ProductProxy;
 
 @RestController
+@RequestMapping("order-controller")
 public class OrderController {
 	
 	@Autowired
