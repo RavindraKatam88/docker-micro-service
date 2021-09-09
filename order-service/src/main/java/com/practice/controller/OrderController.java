9a01f4f8448b864afa218b17c7245143d9bc80d1
RavindraKatam88 <ravindra.katam88@gmail.com>
2021-09-09 21:53:05 +0800

    update order-service

--- order-service/src/main/java/com/practice/controller/OrderController.java
+++ order-service/src/main/java/com/practice/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.practice.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +14,25 @@ import com.practice.proxy.ProductProxy;
 @RestController
 @RequestMapping("order-controller")
 public class OrderController {
-	
+
 	@Autowired
 	private ProductProxy proxy;
-	
+
 	@GetMapping("/product-details")
-	public Product getProduct(){
+	public Product getProduct() {
 		return proxy.getProduct();
 	}
-	
+
 	@GetMapping("/")
-	public String getPing(){
-		return "ping";
+	public String getPing() {
+		return "ping from order controller";
+	}
+
+	@GetMapping("/products")
+	public List<Product> getProducts() {
+		Product product = Product.builder().productId(1).productName("iPhone").quantity(10).build();
+		Product product1 = Product.builder().productId(1).productName("iPhone").quantity(10).build();
+		return Arrays.asList(product, product1);
 	}
 
 }
--- order-service/src/main/java/com/practice/dto/Product.java
+++ order-service/src/main/java/com/practice/dto/Product.java
@@ -1,12 +1,14 @@
 package com.practice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Product {
 
 	private int productId;
