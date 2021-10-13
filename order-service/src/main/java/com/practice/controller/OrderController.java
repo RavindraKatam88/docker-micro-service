1899bf7e740f2a420d4c31c042ec117462f19977
RavindraKatam88 <ravindra.katam88@gmail.com>
2021-10-13 21:36:01 +0800

    added new product

--- order-service/src/main/java/com/practice/controller/OrderController.java
+++ order-service/src/main/java/com/practice/controller/OrderController.java
@@ -1,10 +1,13 @@
 package com.practice.controller;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +17,21 @@ import com.practice.proxy.ProductProxy;
 @RestController
 @RequestMapping("order-controller")
 public class OrderController {
+	private static Map<Integer, Product> dataMap = new HashMap<>();
+	static {
+		dataMap.put(1, Product.builder().productId(1).productName("iPhone").quantity(10).build());
+		dataMap.put(2, Product.builder().productId(2).productName("one plus").quantity(5).build());
+		dataMap.put(3, Product.builder().productId(3).productName("samsung").quantity(12).build());
+		dataMap.put(3, Product.builder().productId(3).productName("oppo").quantity(12).build());
+	}
 
-	@Autowired
+	/*@Autowired
 	private ProductProxy proxy;
 
 	@GetMapping("/product-details")
 	public Product getProduct() {
 		return proxy.getProduct();
-	}
+	}*/
 
 	@GetMapping("/")
 	public String getPing() {
@@ -30,10 +40,13 @@ public class OrderController {
 
 	@GetMapping("/products")
 	public List<Product> getProducts() {
-		Product product = Product.builder().productId(1).productName("iPhone").quantity(10).build();
-		Product product1 = Product.builder().productId(1).productName("iPhone").quantity(10).build();
-		Product product2 = Product.builder().productId(1).productName("samsung").quantity(12).build();
-		return Arrays.asList(product, product1, product2);
+		return new ArrayList<>(dataMap.values());
 	}
+	
+	@GetMapping("/{id}")
+	public Product getProductById(@PathVariable("id") Integer id) {
+		return dataMap.get(id);
+	}
+	
 
 }
